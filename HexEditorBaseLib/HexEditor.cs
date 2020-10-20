1c2f05fa6785f539014c1192543511f1c36075f7
Hakop Zarikyan <hakop2006@gmail.com>
2020-10-19 18:15:17 -0700

    Add project files.

---
+++ HexEditorBaseLib/HexEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HexEditorBaseLib
+{
+    public class HexEditor
+    {
+        //Byte, Hex, Decimal, Char, Binaru
+        public HexEditor()
+        {}
+        public byte[] GetPathBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+        public string[] ConverToHex(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Split('-');
+        }
+        public int[] GetDecimal(byte[] bytes) => bytes.Select(x => (int)x).ToArray();
+        public string[] GetBinary(byte[] bytes) => bytes.Select(x => Convert.ToString()
+        public string[] GetLettersFromHex(string[] hex)
+        {
+            return BitConverter.;
+        }
+    }
+}
